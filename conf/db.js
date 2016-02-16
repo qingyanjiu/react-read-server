7b42a84155010f356a5e69a443f4d848c8d68b30
Louis <qingyanjiu@126.com>
2016-02-16 13:26:58 +0800

    链接mysql数据库

--- conf/db.js
+++ conf/db.js
@@ -3,7 +3,7 @@ module.exports = {
   mysql: {
     host: 'rdsym2yqfamaunmprivate.mysql.rds.aliyuncs.com', 
     user: 'book',
-    password: '19831226lc1',
+    password: '19831226lc',
     database:'book', 
     port: 3306,
     connectionLimit: 10,
