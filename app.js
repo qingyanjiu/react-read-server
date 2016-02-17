ea51a77b2dbd1dd37e04394d8a770b2f91f6d5a1
Louis <qingyanjiu@126.com>
2016-02-17 15:44:40 +0800

    修改了session设置

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(session());
+app.use(session({
+  secret: 'recommand 128 bytes random string', // 建议使用 128 个字符的随机字符串
+  cookie: { maxAge: 60 * 1000 }
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
