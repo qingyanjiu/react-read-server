53977f5b98ec7b454e12b70298a72773609b7a81
alaien <qingyanjiu@126.com>
2016-02-22 17:10:00 +0800

    尝试制作首页界面,添加背景图片

--- public/src/index.js
+++ public/src/index.js
@@ -5,7 +5,7 @@ var Button = require('react-bootstrap').Button;
 
 var jumbotron = (
   <div>
-    <Jumbotron style={{paddingLeft:'2%',backGroundImage:'url(/assets/i/shuji.jpg)'}}>
+    <Jumbotron style={{paddingLeft:'2%',height:'100%'}}>
         <h1>Hello, world!</h1>
         <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
         <p><Button bsStyle="primary">Learn more</Button></p>
