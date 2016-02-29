580711f27d24e5eb14238a053ec0fc14ce7dea2a
alaien <qingyanjiu@126.com>
2016-02-29 15:49:23 +0800

    修改为新数据库

--- conf/db.js
+++ conf/db.js
@@ -1,12 +1,25 @@
 // MySQL数据库联接配置
 module.exports = {
   mysql: {
-    host: 'rdsym2yqfamaunmprivate.mysql.rds.aliyuncs.com', 
-    user: 'book',
-    password: '19831226lc',
-    database:'book', 
+    host: '10.10.11.174', 
+    user: 'uBO2hKbvYlxpWuon',
+    password: 'pDEM5ATbtBZRNpk0F',
+    database:'7WZIbQEn8xNe4SMt', 
     port: 3306,
     connectionLimit: 10,
     supportBigNumbers: true
   }
 };
+
+
+// module.exports = {
+//   mysql: {
+//     host: '10.10.11.174', 
+//     user: 'uBO2hKbvYlxpWuon',
+//     password: 'pDEM5ATbtBZRNpk0F',
+//     database:'7WZIbQEn8xNe4SMt', 
+//     port: 3306,
+//     connectionLimit: 10,
+//     supportBigNumbers: true
+//   }
+// };
