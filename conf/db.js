be9603c33218b228a9bc670d33dd592ea2187e6e
alaien <qingyanjiu@126.com>
2016-03-02 22:07:12 +0800

    daocloud 版本

--- conf/db.js
+++ conf/db.js
@@ -12,28 +12,28 @@
 // };
 
 
-// module.exports = {
-//   mysql: {
-//     host: '10.10.11.174', 
-//     user: 'uBO2hKbvYlxpWuon',
-//     password: 'pDEM5ATbtBZRNpk0F',
-//     database:'7WZIbQEn8xNe4SMt', 
-//     port: 3306,
-//     connectionLimit: 10,
-//     supportBigNumbers: true
-//   }
-// };
-
-
-
 module.exports = {
   mysql: {
-    host: '10.9.1.188', 
-    user: 'up3kuOtL4LVa8ElD',
-    password: 'uwcm2S94gKbZ4rJ9',
-    database:'cf_8463d847_e1f3_48a3_942b_057bceb76f2e', 
+    host: '10.10.11.174', 
+    user: 'uBO2hKbvYlxpWuon',
+    password: 'pDEM5ATbtBZRNpk0F',
+    database:'7WZIbQEn8xNe4SMt', 
     port: 3306,
     connectionLimit: 10,
     supportBigNumbers: true
   }
 };
+
+
+
+// module.exports = {
+//   mysql: {
+//     host: '10.9.1.188', 
+//     user: 'up3kuOtL4LVa8ElD',
+//     password: 'uwcm2S94gKbZ4rJ9',
+//     database:'cf_8463d847_e1f3_48a3_942b_057bceb76f2e', 
+//     port: 3306,
+//     connectionLimit: 10,
+//     supportBigNumbers: true
+//   }
+// };
