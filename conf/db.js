244291d442e782a9b203dc198855c9bc0c70cc9e
alaien <qingyanjiu@126.com>
2016-03-02 21:07:52 +0800

    更新演示平台的数据库地址

--- conf/db.js
+++ conf/db.js
@@ -1,15 +1,15 @@
 // MySQL数据库联接配置
-module.exports = {
-  mysql: {
-    host: 'localhost', 
-    user: 'root',
-    password: '19831226lc',
-    database:'book', 
-    port: 3306,
-    connectionLimit: 10,
-    supportBigNumbers: true
-  }
-};
+// module.exports = {
+//   mysql: {
+//     host: 'localhost', 
+//     user: 'root',
+//     password: '19831226lc',
+//     database:'book', 
+//     port: 3306,
+//     connectionLimit: 10,
+//     supportBigNumbers: true
+//   }
+// };
 
 
 // module.exports = {
@@ -23,3 +23,17 @@ module.exports = {
 //     supportBigNumbers: true
 //   }
 // };
+
+
+
+module.exports = {
+  mysql: {
+    host: '10.9.1.188', 
+    user: 'up3kuOtL4LVa8ElD',
+    password: 'uwcm2S94gKbZ4rJ9',
+    database:'cf_8463d847_e1f3_48a3_942b_057bceb76f2e', 
+    port: 3306,
+    connectionLimit: 10,
+    supportBigNumbers: true
+  }
+};
