efc7f12ab290a3b8e1d9abcb2c4ad962157b8f76
alaien <qingyanjiu@126.com>
2016-03-29 17:00:12 +0800

    修改数据库配置 用于部署

--- conf/db.js
+++ conf/db.js
@@ -1,39 +1,27 @@
 // MySQL数据库联接配置
+
+// module.exports = {
+//   mysql: {
+//     host: 'localhost', 
+//     user: 'root',
+//     password: '19831226lc',
+//     database:'book', 
+//     port: 3306,
+//     connectionLimit: 10,
+//     supportBigNumbers: true
+//   }
+// };
+
+
 module.exports = {
   mysql: {
-    host: 'localhost', 
-    user: 'root',
-    password: '19831226lc',
-    database:'book', 
+    host: '10.10.11.174', 
+    user: 'uBO2hKbvYlxpWuon',
+    password: 'pDEM5ATbtBZRNpk0F',
+    database:'7WZIbQEn8xNe4SMt', 
     port: 3306,
     connectionLimit: 10,
     supportBigNumbers: true
   }
 };
 
-
-// module.exports = {
-//   mysql: {
-//     host: '10.10.11.174', 
-//     user: 'uBO2hKbvYlxpWuon',
-//     password: 'pDEM5ATbtBZRNpk0F',
-//     database:'7WZIbQEn8xNe4SMt', 
-//     port: 3306,
-//     connectionLimit: 10,
-//     supportBigNumbers: true
-//   }
-// };
-
-
-
-// module.exports = {
-//   mysql: {
-//     host: '10.9.1.188', 
-//     user: 'up3kuOtL4LVa8ElD',
-//     password: 'uwcm2S94gKbZ4rJ9',
-//     database:'cf_8463d847_e1f3_48a3_942b_057bceb76f2e', 
-//     port: 3306,
-//     connectionLimit: 10,
-//     supportBigNumbers: true
-//   }
-// };
