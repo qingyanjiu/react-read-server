d49cce4432dabad10a57d085e27d49aa512c609d
alaien <qingyanjiu@126.com>
2016-12-01 15:15:54 +0800

    修改数据库地址

--- conf/db.js
+++ conf/db.js
@@ -1,27 +1,27 @@
 // MySQL数据库联接配置
 
+module.exports = {
+  mysql: {
+    host: 'mysql_test.t0.daoapp.io', 
+    user: 'root',
+    password: '123',
+    database:'book', 
+    port: 61575,
+    connectionLimit: 10,
+    supportBigNumbers: true
+  }
+};
+
+
 // module.exports = {
 //   mysql: {
-//     host: 'localhost', 
-//     user: 'root',
-//     password: '19831226lc',
-//     database:'book', 
+//     host: '10.10.11.174', 
+//     user: 'uBO2hKbvYlxpWuon',
+//     password: 'pDEM5ATbtBZRNpk0F',
+//     database:'7WZIbQEn8xNe4SMt', 
 //     port: 3306,
 //     connectionLimit: 10,
 //     supportBigNumbers: true
 //   }
 // };
-
-
-module.exports = {
-  mysql: {
-    host: '10.10.11.174', 
-    user: 'uBO2hKbvYlxpWuon',
-    password: 'pDEM5ATbtBZRNpk0F',
-    database:'7WZIbQEn8xNe4SMt', 
-    port: 3306,
-    connectionLimit: 10,
-    supportBigNumbers: true
-  }
-};
 
